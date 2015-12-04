151bb63ec12d0cbfccfc46def7a51d5cec27c52e
Alfred <liuchunyao90@126.com>
2015-12-04 21:07:00 +0800

    第一版小游戏代码，添加了跳跃功能

---
+++ Assets/Player.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private float jumpForce = 15;
+	private float gravity = 1.5f;
+	private float speed = 0;
+	private float ground = 0;
+
+	private bool isJumping = false;
+	// Use this for initialization
+	void Start () {
+		ground = transform.position.y;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(isJumping){
+			transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+			speed -= gravity;
+
+			if(transform.position.y <= ground){
+				transform.position += new Vector3(0, ground - transform.position.y, 0);
+				isJumping = false;
+			}
+		}
+	}
+
+	public void Jump(){
+		if(!isJumping){
+			speed = jumpForce;
+			isJumping = true;
+		}
+	}
+}
---
+++ Assets/ScrollGround.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollGround : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position -= new Vector3(Time.deltaTime, 0, 0);
+
+		if(transform.position.x < -11.36f){
+			transform.position = new Vector3(11.36f, transform.position.y, 0);
+		}
+	}
+}
